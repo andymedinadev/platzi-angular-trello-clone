1d6637ebd3f2e542f01f59155b0b271ed2bf3fad
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-01 11:51:32 -0300

    feat: add color model and components for dynamic styling

---
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Color, COLORS } from '@models/color.model';
+
+@Component({
+  selector: 'card-color',
+  templateUrl: './card-color.component.html',
+})
+export class CardColorComponent {
+  @Input() color: Color = 'sky';
+
+  constructor() {}
+
+  mapColors = COLORS;
+
+  get colors() {
+    const classes = this.mapColors[this.color];
+
+    return classes ? classes : {};
+  }
+}
